f7ea56d0056fd723cffa2046b38ca153d94bde67
Rahul Kumar jadi <rahuljadi57@gmail.com>
2025-09-23 15:15:37 +0530

    Your message

--- app.js
+++ app.js
@@ -17,15 +17,31 @@ const User = require('./models/user');
 
 
 
+
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    // useNewUrlParser: true,
-    // useCreateIndex: true,
-    // useUnifiedTopology: true
+// const dbUrl = process.env.DB_URL;
+
+// // mongodb://localhost:27017/yelp-camp
+
+// mongoose.connect(dbUrl, {
+//     // useNewUrlParser: true,
+//     // useCreateIndex: true,
+//     // useUnifiedTopology: true
+//     tlsAllowInvalidCertificates: true
+// });
+
+// const MongoDBStore = require("connect-mongo")(session);
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
+
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
